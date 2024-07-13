bdd1358f56c13ae1cb02ce24b947904666787120
Abu Quatada <abuquatada84@gmail.com>
2024-07-13 12:31:33 +0530

    multi select

--- src/NavPages/Home.jsx
+++ src/NavPages/Home.jsx
@@ -1,12 +1,86 @@
+// import React, { useEffect, useState } from "react";
+// import NavBar from "../NavBar";
+// import Box from "@mui/material/Box";
+// import Plot from "react-plotly.js";
+// import axios from "axios";
+// import base_url from "../utils/API";
+
+// function Home(props) {
+//   const [tableData, setTableData] = useState([]);
+
+//   useEffect(() => {
+//     getData();
+//   }, []);
+
+//   const getData = async () => {
+//     try {
+//       const response = await axios.get(`${base_url}/client/chart/`);
+//       console.log(response.data);
+//       setTableData(response.data);
+//     } catch (err) {
+//       console.error("Error fetching data:", err);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <Box sx={{ display: "flex", p: 10 }}>
+//         <NavBar />
+//         <Box component="main" sx={{ flexGrow: 1 }}>
+//           {/* <h1>Total Sales</h1> */}
+//           <Plot
+//             data={[
+//               {
+//                 x: tableData.due,
+//                 y: tableData.total_amount,
+//                 type: "bar",
+//                 mode: "lines+markers",
+//                 marker: { color: "123270" },
+//               },
+//             ]}
+//             layout={{
+//               title: "TOTAL SALES",
+//               width: 800,
+//               height: 450,
+//               xaxis: { title: "Invoices - Month Wise" },
+//               yaxis: { title: "Amount" },
+//             }}
+//           />
+
+//           <Plot
+//             data={[
+//               {
+//                 labels: tableData.tech_count_name,
+//                 values: tableData.tech_count_num,
+//                 type: "pie",
+//               },
+//             ]}
+//             layout={{
+//               title: "TECHNOLOGIES",
+//               width: 850,
+//               height: 500,
+//               margin: { l: 50, r: 50, b: 100, t: 120 },
+//             }}
+//           />
+//         </Box>
+//       </Box>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
 import React, { useEffect, useState } from "react";
 import NavBar from "../NavBar";
 import Box from "@mui/material/Box";
 import Plot from "react-plotly.js";
 import axios from "axios";
 import base_url from "../utils/API";
+import { Select, MenuItem, FormControl, InputLabel } from "@mui/material";
 
 function Home(props) {
   const [tableData, setTableData] = useState([]);
+  const [selectedChart, setSelectedChart] = useState("technologies");
 
   useEffect(() => {
     getData();
@@ -22,12 +96,15 @@ function Home(props) {
     }
   };
 
+  const handleChartChange = (event) => {
+    setSelectedChart(event.target.value);
+  };
+
   return (
     <div>
       <Box sx={{ display: "flex", p: 10 }}>
         <NavBar />
         <Box component="main" sx={{ flexGrow: 1 }}>
-          {/* <h1>Total Sales</h1> */}
           <Plot
             data={[
               {
@@ -47,21 +124,55 @@ function Home(props) {
             }}
           />
 
-          <Plot
-            data={[
-              {
-                labels: tableData.tech_count_name,
-                values: tableData.tech_count_num,
-                type: "pie",
-              },
-            ]}
-            layout={{
-              title: "TECHNOLOGIES",
-              width: 850,
-              height: 500,
-              margin: { l: 50, r: 50, b: 100, t: 120 },
-            }}
-          />
+          <FormControl fullWidth sx={{ marginTop: 5 }}>
+            <InputLabel id="select-chart-label">Select Chart</InputLabel>
+            <br />
+            <Select
+              labelId="select-chart-label"
+              id="select-chart"
+              value={selectedChart}
+              onChange={handleChartChange}
+            >
+              <MenuItem value="technologies">Technologies</MenuItem>
+              <MenuItem value="anotherChart">Another Chart</MenuItem>
+            </Select>
+          </FormControl>
+
+          {selectedChart === "technologies" && (
+            <Plot
+              data={[
+                {
+                  labels: tableData.tech_count_name,
+                  values: tableData.tech_count_num,
+                  type: "pie",
+                },
+              ]}
+              layout={{
+                title: "TECHNOLOGIES",
+                width: 850,
+                height: 500,
+                margin: { l: 50, r: 50, b: 100, t: 120 },
+              }}
+            />
+          )}
+
+          {selectedChart === "anotherChart" && (
+            <Plot
+              data={[
+                {
+                  labels: tableData.due_date,
+                  values: tableData.total_amount,
+                  type: "pie",
+                },
+              ]}
+              layout={{
+                title: "ANOTHER CHART",
+                width: 850,
+                height: 500,
+                margin: { l: 50, r: 50, b: 100, t: 120 },
+              }}
+            />
+          )}
         </Box>
       </Box>
     </div>
--- src/NavPages/Project.jsx
+++ src/NavPages/Project.jsx
@@ -15,19 +15,24 @@ import {
   FormControl,
   InputLabel,
   Modal,
+  Select,
+  MenuItem,
+  Checkbox,
+  ListItemText,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import NavBar from "../NavBar";
 import axios from "axios";
 import base_url from "../utils/API";
-function Project(props) {
+
+const Project = (props) => {
   const initialFormData = {
     project_name: "",
     duration: "",
     client_id: "",
     team_id: "",
-    tech_id: "",
+    tech_id: [],
   };
 
   const [formData, setFormData] = useState(initialFormData);
@@ -35,10 +40,36 @@ function Project(props) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [editMode, setEditMode] = useState(false);
   const [editIndex, setEditIndex] = useState(null);
+  const [techOptions, setTechOptions] = useState([]);
+  const [client, setClient] = useState([]);
+  const [team, setTeam] = useState([]);
+  const [client_id, setClient_id] = useState("");
+  const [team_id, setTeam_id] = useState("");
 
   useEffect(() => {
     getData();
+    getClient();
+    getTeam();
+    fetchTechOptions();
   }, []);
+
+  const getClient = async () => {
+    try {
+      const response = await axios.get(`${base_url}/client/client/`);
+      setClient(response.data);
+    } catch (err) {
+      console.error("Error fetching data:", err);
+    }
+  };
+
+  const getTeam = async () => {
+    try {
+      const response = await axios.get(`${base_url}/client/team/`);
+      setTeam(response.data);
+    } catch (err) {
+      console.error("Error fetching data:", err);
+    }
+  };
 
   const getData = async () => {
     try {
@@ -50,26 +81,46 @@ function Project(props) {
     }
   };
 
-  function postDataToServer(values) {
-    axios
-      .post(`${base_url}/client/project/`, formData)
-      .then((res) => {
-        console.log(res.data);
-        getData();
-        alert("Project Added Successfully");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+  const fetchTechOptions = async () => {
+    try {
+      const response = await axios.get(`${base_url}/client/api/technology/`);
+      console.log(response.data, "111111111");
+      setTechOptions(
+        response.data.map((tech) => ({
+          value: tech.tech_id,
+          label: tech.name,
+        }))
+      );
+    } catch (err) {
+      console.error("Error fetching tech options:", err);
+    }
+  };
+
+  const postDataToServer = async () => {
+    try {
+      await axios.post(`${base_url}/client/project/`, formData);
+      getData();
+      alert("Project Added Successfully");
+    } catch (err) {
+      console.error("Error adding project:", err);
+    }
+  };
 
   const updateDataToServer = async () => {
     try {
-      await axios.put(`${base_url}/client/project/`, formData);
+      const updatedFormData = {
+        ...formData,
+        tech_id: formData.tech_id.map((tech) =>
+          typeof tech === "string"
+            ? techOptions.find((option) => option.label === tech).value
+            : tech
+        ),
+      };
+      await axios.put(`${base_url}/client/project/`, updatedFormData);
       getData();
       alert("Project updated Successfully");
     } catch (err) {
-      console.error("Error updating client:", err);
+      console.error("Error updating project:", err);
     }
   };
 
@@ -79,7 +130,7 @@ function Project(props) {
       getData();
       alert("Project deleted Successfully");
     } catch (err) {
-      console.error("Error deleting client:", err);
+      console.error("Error deleting project:", err);
     }
   };
 
@@ -100,6 +151,32 @@ function Project(props) {
     setFormData({
       ...formData,
       [name]: value,
+    });
+  };
+
+  const handleTechChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setFormData({
+      ...formData,
+      tech_id: typeof value === "string" ? value.split(",") : value,
+    });
+  };
+
+  const handleChangeClientDropdown = (event) => {
+    setClient_id(event.target.value);
+    setFormData({
+      ...formData,
+      client_id: +event.target.value,
+    });
+  };
+
+  const handleChangeTeamDropdown = (event) => {
+    setTeam_id(event.target.value);
+    setFormData({
+      ...formData,
+      team_id: +event.target.value,
     });
   };
 
@@ -172,7 +249,7 @@ function Project(props) {
             {editMode ? "Edit Project" : "Add Project"}
           </Typography>
           <FormControl sx={{ margin: 2 }}>
-            <InputLabel htmlFor="project-id">Project Name</InputLabel>
+            <InputLabel htmlFor="project-name">Project Name</InputLabel>
             <Input
               id="project-name"
               name="project_name"
@@ -189,106 +266,115 @@ function Project(props) {
               onChange={handleChange}
             />
           </FormControl>
-          <FormControl sx={{ margin: 2 }}>
-            <InputLabel htmlFor="client-id">Client Id</InputLabel>
-            <Input
-              id="client-id"
-              name="client_id"
-              value={formData.client_id}
-              onChange={handleChange}
-            />
+          <FormControl sx={{ margin: 2, width: 200 }}>
+            <InputLabel id="demo-simple-select-label">Client Name</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={client_id}
+              variant="standard"
+              label="Client Name"
+              onChange={handleChangeClientDropdown}
+            >
+              {client.map((row, index) => (
+                <MenuItem key={index} value={row.client_id}>
+                  {row.client_name}
+                </MenuItem>
+              ))}
+            </Select>
           </FormControl>
-          <FormControl sx={{ margin: 2 }}>
+          <FormControl sx={{ margin: 2, width: 200 }}>
             <InputLabel htmlFor="team-id">Team Id</InputLabel>
-            <Input
-              id="team-id"
-              name="team_id"
-              value={formData.team_id}
-              onChange={handleChange}
-            />
+            <Select
+              labelId="demo-simple-select-label"
+              id="team"
+              value={team_id}
+              variant="standard"
+              label="Team"
+              onChange={handleChangeTeamDropdown}
+            >
+              {team.map((row, index) => (
+                <MenuItem key={index} value={row.team_id}>
+                  {row.team_name}
+                </MenuItem>
+              ))}
+            </Select>
           </FormControl>
-          <FormControl sx={{ margin: 2 }}>
+          <FormControl sx={{ margin: 2, width: 200 }}>
             <InputLabel htmlFor="tech-id">Tech Id</InputLabel>
-            <Input
+            <Select
+              labelId="tech-id-label"
               id="tech-id"
-              name="tech_id"
+              label="tech_id"
+              multiple
               value={formData.tech_id}
-              onChange={handleChange}
-            />
+              onChange={handleTechChange}
+              renderValue={(selected) =>
+                selected
+                  .map((techId) => {
+                    const tech = techOptions.find(
+                      (option) => option.value === techId
+                    );
+                    return tech ? tech.label : techId;
+                  })
+                  .join(", ")
+              }
+            >
+              {techOptions.map((tech) => (
+                <MenuItem key={tech.value} value={tech.value}>
+                  <Checkbox checked={formData.tech_id.includes(tech.value)} />
+                  <ListItemText primary={tech.label} />
+                </MenuItem>
+              ))}
+            </Select>
           </FormControl>
-          <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
-            <Button variant="contained" color="success" onClick={handleSubmit}>
-              {editMode ? "Update" : "Save"}
-            </Button>
-            <Button variant="outlined" color="error" onClick={handleCloseModal}>
-              Cancel
+          <Box sx={{ margin: 2, display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              onClick={handleSubmit}
+              size="large"
+              variant="contained"
+              sx={{
+                color: "white",
+                backgroundColor: "#123270",
+                borderRadius: 2,
+                "&:hover": { color: "black", backgroundColor: "#53B789" },
+              }}
+            >
+              {editMode ? "Update" : "Add"}
             </Button>
           </Box>
         </Box>
       </Modal>
-
-      <TableContainer component={Paper} sx={{ marginTop: 5 }}>
-        <Table>
-          <TableHead sx={{ m: 5, backgroundColor: "#53B789" }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
             <TableRow>
-              <TableCell sx={{ color: "white", textAlign: "center" }}>
-                Project Name
-              </TableCell>
-              <TableCell sx={{ color: "white", textAlign: "center" }}>
-                Duration
-              </TableCell>
-              <TableCell sx={{ color: "white", textAlign: "center" }}>
-                Client Id
-              </TableCell>
-              <TableCell sx={{ color: "white", textAlign: "center" }}>
-                Team Id
-              </TableCell>
-              <TableCell sx={{ color: "white", textAlign: "center" }}>
-                Tech Id
-              </TableCell>
-              <TableCell sx={{ color: "white", textAlign: "center" }}>
-                Action
-              </TableCell>
+              <TableCell>Project Name</TableCell>
+              <TableCell>Duration</TableCell>
+              <TableCell>Client Name</TableCell>
+              <TableCell>Team Name</TableCell>
+              <TableCell>Tech Name</TableCell>
+              <TableCell>Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {tableData.map((row, index) => (
-              <TableRow
-                key={index}
-                sx={{
-                  m: 5,
-                  height: "3",
-                  backgroundColor: "#fff",
-                  "&:hover": { backgroundColor: "#dcf0e7" },
-                }}
-              >
-                <TableCell sx={{ textAlign: "center" }}>
-                  {row.project_name}
-                </TableCell>
-                <TableCell sx={{ textAlign: "center" }}>
-                  {row.duration}
-                </TableCell>
-                <TableCell sx={{ textAlign: "center" }}>
-                  {row.client_id}
-                </TableCell>
-                <TableCell sx={{ textAlign: "center" }}>
-                  {row.team_id}
-                </TableCell>
-                <TableCell sx={{ textAlign: "center" }}>
-                  {row.tech_id}
-                </TableCell>
-                <TableCell sx={{ textAlign: "center" }}>
+              <TableRow key={index}>
+                <TableCell>{row.project_name}</TableCell>
+                <TableCell>{row.duration}</TableCell>
+                <TableCell>{row.client_name}</TableCell>
+                <TableCell>{row.team_name}</TableCell>
+                <TableCell>{row.tech_id.join(", ")}</TableCell>
+                <TableCell>
                   <IconButton
-                    onClick={() => handleEdit(index)}
                     aria-label="edit"
-                    sx={{ color: "grey" }}
+                    onClick={() => handleEdit(index)}
                   >
                     <EditIcon />
                   </IconButton>
                   <IconButton
-                    onClick={() => handleDelete(row.project_id)}
                     aria-label="delete"
-                    sx={{ color: "red" }}
+                    onClick={() => handleDelete(row.project_id)}
                   >
                     <DeleteIcon />
                   </IconButton>
@@ -300,6 +386,6 @@ function Project(props) {
       </TableContainer>
     </Box>
   );
-}
+};
 
 export default Project;
